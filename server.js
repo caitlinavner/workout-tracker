fcd44067b2656911283c6b023d10e80bf6183c78
cavner <katieavner@gmail.com>
2020-03-23 22:20:11 -0700

    fixed routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -13,15 +13,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
 });
 
-//link to routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes.view.js"));
-
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`Server listening on: http://localhost:${PORT} !`);
 });
